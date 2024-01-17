d4b95420e8fa2c28800b820465eb570dea0ce72c
a.korzhenevskaya <7_korzhenevskaia_aa@bts.declarant.by>
2024-01-17 16:06:09 +0300

    fix: исправлена ошибка с jquery

--- customs/src/main/resources/static/js/app.js
+++ customs/src/main/resources/static/js/app.js
@@ -97,111 +97,123 @@ function registrateUser() {
 
 function loadDbStructure() {
     $.ajax({
-            url: '/api/queries/allById/3',
-            method: 'get',
-            dataType: 'application/json',
-            data: {"userId": 3},
-            success: function (data) {
-                document.getElementById("sqlHistory").value += data;
-            }
-        });
+        url: '/api/queries/allById/3',
+        method: 'get',
+        dataType: 'application/json',
+        data: {"userId": 3},
+        success: function (data) {
+            document.getElementById("sqlHistory").value += data;
+        }
+    });
 }
 
 function makeQuery() {
-    document.getElementById("sqlText").value='';
-        var database = document.getElementById("databases").value;
-        var selectMargin = document.getElementById("select").value;
-        var distinctSwitch = document.getElementById("distinct").checked;
-        var distinctTrue;
-        if(distinctSwitch==true) { distinctTrue = ' DISTINCT'; }
-        else { distinctTrue = ''; }
-        var fromMargin = document.getElementById("from").value;
-        var whereMargin = document.getElementById("where").value;
-        var andSwitch = document.getElementById("and").checked;
-        var andTrue;
-        if(andSwitch==true) { andTrue = ' AND'; }
-        else { andTrue = ''; }
-        var groupByMargin = document.getElementById("groupBy").value;
-        var groupByText = '';
-        if(groupByMargin!='') { groupByText = ' GROUP BY ' + groupByMargin; }
-        var orderByMargin = document.getElementById("orderBy").value;
-        var orderByText = '';
-        if(orderByMargin!='') { orderByText = ' ORDER BY ' + orderByMargin; }
-
-        if(selectMargin=='' || fromMargin=='' || whereMargin=='') { alert('Заполните обязательные поля!'); }
-        else {
-            var query = 'SELECT ' + selectMargin + distinctTrue + ' FROM ' + fromMargin + ' WHERE ' + whereMargin +
-                andTrue + groupByText + orderByText + ';'; }
-
-        document.getElementById("sqlText").value=(query);
+    document.getElementById("sqlText").value = '';
+    var database = document.getElementById("databases").value;
+    var selectMargin = document.getElementById("select").value;
+    var distinctSwitch = document.getElementById("distinct").checked;
+    var distinctTrue;
+    if (distinctSwitch == true) {
+        distinctTrue = ' DISTINCT';
+    } else {
+        distinctTrue = '';
+    }
+    var fromMargin = document.getElementById("from").value;
+    var whereMargin = document.getElementById("where").value;
+    var andSwitch = document.getElementById("and").checked;
+    var andTrue;
+    if (andSwitch == true) {
+        andTrue = ' AND';
+    } else {
+        andTrue = '';
+    }
+    var groupByMargin = document.getElementById("groupBy").value;
+    var groupByText = '';
+    if (groupByMargin != '') {
+        groupByText = ' GROUP BY ' + groupByMargin;
+    }
+    var orderByMargin = document.getElementById("orderBy").value;
+    var orderByText = '';
+    if (orderByMargin != '') {
+        orderByText = ' ORDER BY ' + orderByMargin;
     }
 
-    function saveQuery() {
-        let sqlText = $('#sqlText').val();
-        console.log('sql query text = ' + sqlText + ' user id = ' + userIdOutput);
-        $.ajax({
-            type: 'POST',
-            url: '/api/queries/save',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                "userId": userIdOutput,
-                "queryDefinition": sqlText
-            }),
-            success: function (response) {
-                console.log('Запрос сохранен в бд:', response);
-                alert('Запрос сохранен в бд.');
-                document.getElementById("sqlHistory").value += sqlText + '\n';
-            },
-            error: function (error) {
-                console.error('Ошибка в сохранении запроса в бд:', error);
-                alert('Ошибка в сохранении запроса в бд');
-            }
+    if (selectMargin == '' || fromMargin == '' || whereMargin == '') {
+        alert('Заполните обязательные поля!');
+    } else {
+        var query = 'SELECT ' + selectMargin + distinctTrue + ' FROM ' + fromMargin + ' WHERE ' + whereMargin +
+            andTrue + groupByText + orderByText + ';';
+    }
+
+    document.getElementById("sqlText").value = (query);
+}
+
+function saveQuery() {
+    let sqlText = $('#sqlText').val();
+    console.log('sql query text = ' + sqlText + ' user id = ' + userIdOutput);
+    $.ajax({
+        type: 'POST',
+        url: '/api/queries/save',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "userId": userIdOutput,
+            "queryDefinition": sqlText
+        }),
+        success: function (response) {
+            console.log('Запрос сохранен в бд:', response);
+            alert('Запрос сохранен в бд.');
+            document.getElementById("sqlHistory").value += sqlText + '\n';
+        },
+        error: function (error) {
+            console.error('Ошибка в сохранении запроса в бд:', error);
+            alert('Ошибка в сохранении запроса в бд');
+        }
     })
-    }
+}
 
 function executeQuery() {
     let sqlText = $('#sqlText').val();
     console.log('sql query text = ' + sqlText + ' user id = ' + userIdOutput);
-        $.ajax({
-            url: '/api/operations/setsqlquery',
-            method: 'get',
-            dataType: 'application/json',
-            data: {"query": sqlText},
-            success: function (data) {
-                console.log("Success" + data);
-                $('#queryResults').val() = data;
-            }
-        });
+    $.ajax({
+        url: '/api/operations/setsqlquery',
+        method: 'get',
+        dataType: 'application/json',
+        data: {"query": sqlText},
+        success: function (data) {
+            console.log("Success" + data);
+            $('#queryResults').val(data);
+        }
+    });
 }
 
 function showPieDiagram() {
     anychart.onDocumentReady(function () {
-                // create pie chart with passed data
-                var data = anychart.data.set([
-                  ["значение 1", 69.2, 24.2],
-                  ["значение 2", 85, 334],
-                  ["значение 3", 32.1, 1045],
-                  ["значение 4", 8.2, 3038],
-                ]);
+        // create pie chart with passed data
+        var data = anychart.data.set([
+            ["значение 1", 69.2, 24.2],
+            ["значение 2", 85, 334],
+            ["значение 3", 32.1, 1045],
+            ["значение 4", 8.2, 3038],
+        ]);
 
-                var wealth = data.mapAs({ x: 0, value: 1 });
+        var wealth = data.mapAs({x: 0, value: 1});
 
-                var chart = anychart.pie(wealth);
-                chart
-                  .labels()
-                  .hAlign("center")
-                  .position("outside")
-                  .format("{%Value} trn.n({%PercentOfCategory}%)");
-                chart
-                  .legend()
-                  // set legend position and items layout
-                  .position("right")
-                  .itemsLayout("vertical")
-                  .align("center");
+        var chart = anychart.pie(wealth);
+        chart
+            .labels()
+            .hAlign("center")
+            .position("outside")
+            .format("{%Value} trn.n({%PercentOfCategory}%)");
+        chart
+            .legend()
+            // set legend position and items layout
+            .position("right")
+            .itemsLayout("vertical")
+            .align("center");
 
-                // set container id for the chart
-                chart.container("container");
-                // initiate chart drawing
-                chart.draw();
-              });
+        // set container id for the chart
+        chart.container("container");
+        // initiate chart drawing
+        chart.draw();
+    });
 }
